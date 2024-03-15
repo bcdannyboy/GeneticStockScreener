add03b3922cf43b3d2dff48febe195accbfdec0f
Daniel Bloom <daniel@redteamarts.com>
2024-03-15 15:08:19 -0700

    update

--- src/genetic/init.go
+++ src/genetic/init.go
@@ -62,6 +62,7 @@ func (ga *GA) RunGeneticAlgorithm() (*genetic_weight.Weight, float64, float64, f
 
 		if best >= lastBest {
 			if best > bestPortfolioScore {
+				fmt.Println("New best portfolio score found")
 				bestPortfolioScore = best
 				topWeight = topW
 				secondaryStagnation = 0
@@ -70,6 +71,7 @@ func (ga *GA) RunGeneticAlgorithm() (*genetic_weight.Weight, float64, float64, f
 			}
 			stagnation = 0 // Reset stagnation
 		} else {
+			fmt.Printf("Stagnation detected (Best: %f, Last Best: %f)\n", best, lastBest)
 			stagnation++ // Increment stagnation
 		}
 
@@ -81,8 +83,8 @@ func (ga *GA) RunGeneticAlgorithm() (*genetic_weight.Weight, float64, float64, f
 		ga.MutationRate = 0.01 + rand.Float64()*0.1 + (rand.Float64() * float64(stagnation) / float64(ga.AcceptableStagnation))
 		ga.CrossoverRate = 0.8 + rand.Float64()*0.1 + (rand.Float64() * float64(stagnation) / float64(ga.AcceptableStagnation))
 
-		fmt.Printf("Generation (%d/%d): Generation's best: %f, Generation's Worst: %f, Total Best: %f, Total Worst: %f, Mutation Rate: %f, Crossover Rate: %f, Stagnation: %d/%d, TopRateStagnation: %d/%d\n",
-			i, ga.Generations, best, worst, bestPortfolioScore, worstPortfolioScore, ga.MutationRate, ga.CrossoverRate, stagnation, ga.AcceptableStagnation, secondaryStagnation, int(float64(ga.Generations)*float64(0.75)))
+		fmt.Printf("Generation (%d/%d): Generation's best: %f, last Best: %f, Generation's Worst: %f, Total Best: %f, Total Worst: %f, Mutation Rate: %f, Crossover Rate: %f, Stagnation: %d/%d, TopRateStagnation: %d/%d\n",
+			i, ga.Generations, best, lastBest, worst, bestPortfolioScore, worstPortfolioScore, ga.MutationRate, ga.CrossoverRate, stagnation, ga.AcceptableStagnation, secondaryStagnation, int(float64(ga.Generations)*float64(0.75)))
 
 		if stagnation >= ga.AcceptableStagnation || float64(secondaryStagnation) >= (float64(ga.Generations)*float64(0.75)) { // if stagnation of best solution happens for over 60% of population size or stagnation of the local solutions happens for ga.AcceptableStagnation generations
 			fmt.Println("Stagnation detected, introducing more diversity")
